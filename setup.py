e77cf32cc44a3188286f9ab2f4eb74d192d1e9c3
Yiannis Giannelos <jgiannelos@wikimedia.org>
2021-09-10 14:28:19 +0300

    Change project structure and add packaging setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="maps-simplified-tilelist",
+    version="0.0.1",
+    author="Yiannis Giannelos",
+    author_email="jgiannelos@wikimedia.org",
+    description="Generate a distinct list of map tiles up to a zoom level",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.7"
+)
